99b281258dafb0bef6acda9e5499d91ce88edace
Jie-Mac <e25254@gmail.com>
2022-10-21 13:54:29 +0800

    socket server

---
+++ routes/ws-echo.js
@@ -0,0 +1,13 @@
+const WebSocket = require("ws");
+
+const createEchoServer = (server) => {
+	const wsServer = new WebSocket.Server({ server });
+	wsServer.on("connection", (ws, req) => {
+		ws.send("-----");
+		ws.on("message", (message) => {
+			ws.send(message.toString());
+		});
+	});
+};
+
+module.exports = createEchoServer;
